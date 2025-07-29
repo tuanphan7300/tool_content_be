1824a602e715635cce11bb1e7d1c61ad5aa29952
huongnq2yopaz <huongnq2@yopaz.vn>
2025-07-29 15:03:37 +0700

    update

--- main.go
+++ main.go
@@ -79,7 +79,7 @@ func main() {
 
 	r := gin.Default()
 	r.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"*"},
+		AllowOrigins:     []string{"https://inis-hvnh.site", "*"},
 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
 		AllowCredentials: true,
